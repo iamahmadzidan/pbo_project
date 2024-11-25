3ba9ccc6bcc9eb3ec0508e7094ede55a488ee7a7
amrullh <168280074+amrullh@users.noreply.github.com>
2024-11-26 02:03:50 +0800

    atur login pimpinan
    hai nigga

--- lib/halaman/home_page.dart
+++ lib/halaman/home_page.dart
@@ -1,34 +1,52 @@
 import 'package:flutter/material.dart';
+import 'activity_page.dart';
+import 'profile_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const Center(child: Text("Home Content")),
+    const ActivityPage(),
+    const ProfilePage(),
+  ];
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Column(
-        children: [
-          const SizedBox(height: 22,),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                Text('Welcome back!'),
-                SizedBox(height: 2),
-                Text(
-                  "Bruce wyne T", 
-                  style: TextStyle(fontSize: 25,
-                  fontWeight: FontWeight.bold),
-                
-                  )
-              ]
-            ),
-          )
-
+      appBar: AppBar(title: const Text("Welcome back!")),
+      body: _pages[_currentIndex], // Menampilkan konten sesuai tab yang dipilih
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Beranda',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'Aktivitas',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
         ],
-      )
       ),
     );
-    
   }
 }

ceabd0773dc482af29ee43b77bac2312bfcacbc1
iamahmadzidan <achmadzidann219@gmail.com>
2024-11-26 02:04:08 +0800

    nigga1
    nigga 2

--- lib/halaman/profile_page.dart
+++ lib/halaman/profile_page.dart
@@ -8,9 +8,70 @@ class ProfilePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Profil'),
+        backgroundColor: const Color(0xFF099AF5), // Warna biru sesuai kode
       ),
-      body: const Center(
-        child: Text('Ini adalah halaman Profil'),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // ID Card
+            Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(16),
+                width: double.infinity,
+                height: 120,
+                child: Row(
+                  children: [
+                    // Foto Profil
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width *
+                          0.3, // 2/5 dari lebar ID card
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.asset(
+                          'assets/image/profilepicture.jpg', // Gambar lokal
+                          fit: BoxFit.cover,
+                          height: double.infinity,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 16),
+                    // Biodata Singkat
+                    const Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Nama: Fadhil Hilmy',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Umur: 19 tahun',
+                            style: TextStyle(fontSize: 14),
+                          ),
+                          SizedBox(height: 8),
+                          Text(
+                            'Email: zidan@example.com',
+                            style: TextStyle(fontSize: 14),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
